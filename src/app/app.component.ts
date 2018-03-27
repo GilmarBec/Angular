92a3fe03f38e3a67ac94e19ee3da0d1d328007c2
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-03-27 08:58:42 -0300

    Hello World

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Hello World!';
+  message = 'My First Angular App!';
 }
